6fecb8814b9507da35d926b8f84713929da6474f
Patrick Raspante <praspant@redhat.com>
2022-03-12 15:39:53 -0600

    testing

--- scripts/login_argocd.sh
+++ scripts/login_argocd.sh
@@ -2,6 +2,8 @@
 
 #PASS="$1"
 
-PASS="EAyP49Z0Knm7Twh5DkHuMzXaoUdFWVxG"
+ARGO_SERVER="openshift-gitops-server-openshift-gitops.apps.pwrocp4.lab.upshift.rdu2.redhat.com"
+ADMIN_USER="admin"
+PASS=$(oc get secret openshift-gitops-cluster -n openshift-gitops -o=jsonpath='{ .data.admin\.password }' | base64 -d)
 
-argocd login openshift-gitops-server-openshift-gitops.apps.pwrocp4.lab.upshift.rdu2.redhat.com --username admin --password ${PASS} --insecure
+argocd login ${ARGO_SERVER} --username ${ADMIN_USER} --password ${PASS} --insecure
