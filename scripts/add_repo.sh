beaef8878c2cd5dc3720a727386cb820f71b568f
Patrick Raspante <praspant@redhat.com>
2022-03-19 15:22:26 -0500

    weird key bug

--- scripts/add_repo.sh
+++ scripts/add_repo.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 REPO="git@github.com:praspa/ocp4-gitops-demo.git"
-KEY_PATH="/home/praspant/.ssh/id_rsa"
+KEY_PATH="/home/praspant/.ssh/id_ed25519"
 
 argocd repo add "${REPO}" --insecure-ignore-host-key --ssh-private-key-path "${KEY_PATH}" --insecure 
 
